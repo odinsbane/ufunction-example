33b08d6859696673f8a5a2efb123fc92820e220e
m smith <matthew.smith3@crick.ac.uk>
2018-09-13 11:56:25 +0100

    Added element-wise check.

--- package/check.py
+++ package/check.py
@@ -4,7 +4,13 @@ import numpy
 import sys
 
 
+
+
+
 def fun1(x, y):
+    """
+        Generic function with multiple numpy ufunctions.
+    """
     return x**3 + x**2*y + x*y**2 + y**3 - 0.1
 
 def fun3(x,y):
@@ -20,6 +26,18 @@ def fun3(x,y):
     numpy.add(T, -0.1, T)
     return T
 
+def fun4(x,y):
+    """
+        element wise version.
+    """
+    w = len(x)
+    h = len(x[0])
+    z = [ [0]*w for i in range(h) ]
+    for i in range(w):
+        for j in range(h):
+            z[i][j] = x[i][j]**3 + x[i][j]**2*y[i][j] + x[i][j]*y[i][j]**2 + y[i][j]**3 - 0.1
+    return z
+
 numpy.random.seed(1)
 x = numpy.random.random((10000, 10000))
 y = numpy.random.random((10000, 10000))
@@ -27,5 +45,8 @@ y = numpy.random.random((10000, 10000))
 for i in range(5):
     x = fun1(x,y)
 
-print(sum(sum(x)))
+if isinstance(x, list):
+    print(sum(i for r in x for i in r))
+else:
+    print(sum(sum(x)))
 
